0c833ebf500f6d048eb5af5c76e5fe09e59a447f
leonardo-schneider2 <leocoelho256@gmail.com>
2022-08-05 19:49:38 -0300

    bora

--- src/App.js
+++ src/App.js
@@ -4,23 +4,18 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Header from './components/Header';
 import Game from './pages/Game';
-import Settings from '.pages/Settings';
+import Settings from './pages/Settings';
 // import logo from './trivia.png';
 
 export default function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={ logo } className="App-logo" alt="logo" />
-    //     <p>SUA VEZ</p>
-    //   </header>
-    // </div>
     <>
       <Header />
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ Game } />
         <Route path="/settings" component={ Settings } />
-    </Switch>
+      </Switch>
+    </>
   );
 }
