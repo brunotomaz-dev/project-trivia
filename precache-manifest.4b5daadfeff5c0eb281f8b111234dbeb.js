89e68b43aa11c7df43418eefded8992db8d26b5a
brunotomaz-dev <tomaz.bruno@gmail.com>
2022-10-23 21:37:25 -0300

    Updates

---
+++ precache-manifest.4b5daadfeff5c0eb281f8b111234dbeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f40e37036c9c3d4d4a8b71ce2ce4c3e",
+    "url": "/project-trivia/index.html"
+  },
+  {
+    "revision": "1016d0c1513a3721cc71",
+    "url": "/project-trivia/static/css/main.4245ca43.chunk.css"
+  },
+  {
+    "revision": "0c5b38eeb13162bed92c",
+    "url": "/project-trivia/static/js/2.3c7db5ca.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/project-trivia/static/js/2.3c7db5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1016d0c1513a3721cc71",
+    "url": "/project-trivia/static/js/main.7e781ec1.chunk.js"
+  },
+  {
+    "revision": "f125377fa2c39526d4b3",
+    "url": "/project-trivia/static/js/runtime-main.33b57c0b.js"
+  }
+]);
